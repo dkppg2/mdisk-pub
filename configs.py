5f0eb5d83d9cf6606db3476bbe7d6b482f6c681b
dkppg <117081087+dkppg@users.noreply.github.com>
2022-12-19 17:31:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,11 +3,11 @@ import os
 
 class Config(object):
     API_ID = os.environ.get("API_ID", "15050363")
-    API_HASH = os.environ.get("API_HASH", "209169a882ff43c4f1621b7cc97c255b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5544101630:AAG1IB4ASSo2iRNy7NE09qOzcos6PkqCx00")
+    API_HASH = os.environ.get("API_HASH", "a8ee65e5057b3f05cf9f28b71667203a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5433519682:AAGalXAhE4h2FuVBTnJTsyIOPo6xYgH1UwY")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Mdisk-Bot")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001679764868")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001691956031")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
@@ -15,7 +15,7 @@ class Config(object):
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://john-abu11:ooz4PXFbWBV3OLIs@cluster0.mhlqpbe.mongodb.net/?retryWrites=true&w=majority")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 660755963))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 2067727305))
 
     START_TEXT = """
 Hi Unkil, I am Mdisk Bot!
